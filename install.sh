b2dd2072ed1194c2fe4aa16711a7f917e5c68d67
Matthias Richter <matthias.richter@identpro.de>
2018-01-25 12:51:53 +0100

    removed most of macos stuff

--- install.sh
+++ install.sh
@@ -23,18 +23,6 @@ ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
-# Package managers & packages
-
-. "$DOTFILES_DIR/install/brew.sh"
-. "$DOTFILES_DIR/install/npm.sh"
-. "$DOTFILES_DIR/install/bash.sh"
-. "$DOTFILES_DIR/install/brew-cask.sh"
-. "$DOTFILES_DIR/install/gem.sh"
-
-# Run tests
-
-if is-executable bats; then bats test/*.bats; else echo "Skipped: tests (missing: bats)"; fi
-
 # Install extra stuff
 
 if [ -d "$DOTFILES_EXTRA_DIR" -a -f "$DOTFILES_EXTRA_DIR/install.sh" ]; then
